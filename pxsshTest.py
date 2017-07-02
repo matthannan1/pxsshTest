be2d352e509b38c96671cdebe11afcde4e05be24
Matt Hannan <matthannan1@gmail.com>
2017-07-01 22:39:38 -0400

    Have a file. Won't work on Windows.

---
+++ pxsshTest.py
@@ -0,0 +1,23 @@
+#import pxssh
+import pexpect
+import getpass
+try:                                
+    s = pxssh.pxssh()
+    hostname = raw_input('hostname: ')
+    username = raw_input('username: ')
+    password = getpass.getpass('password: ')
+ #   port = 21098
+    s.login (hostname, username, password, port = 21098)
+    s.sendline ('uptime')  # run a command
+    s.prompt()     # match the prompt
+    print s.before     # print everything before the prompt.
+    s.sendline ('ls -l')
+    s.prompt()
+    print s.before
+    s.sendline ('df')
+    s.prompt()
+    print s.before
+    s.logout()
+except pxssh.ExceptionPxssh, e:
+    print "pxssh failed on login."
+    print str(e)
